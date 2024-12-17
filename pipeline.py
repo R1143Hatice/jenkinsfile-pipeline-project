4ba62cb08cf0595f0026686a9fe648cdc2ccb63e
EC2 Default User <ec2-user@ip-172-31-77-200.ec2.internal>
2024-12-17 20:39:10 +0000

    updated jenkinsfile and added pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -9,3 +9,15 @@ pipeline {
         }
     }
 }
+pipeline {
+    agent any
+    stages {
+        stage('run') {
+            steps {
+                echo 'Clarusway_Way to Reinvent Yourself'
+                sh 'python3 --version'
+                sh 'python3 pipeline.py'
+            }
+        }
+    }
+}
